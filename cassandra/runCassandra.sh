73bf24b82438593d73465cb8a8dfb00695dee3df
Taras Moisiuk <taras.moisiuk@kohls.com>
2020-02-22 14:15:15 +0300

    Cassandra config fix

--- cassandra/runCassandra.sh
+++ cassandra/runCassandra.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-docker run --name cassandra \
-    -p 7000:7000  \
-    bitnami/cassandra:latest
+docker network create cassandranetwork
 
+docker run --name some-cassandra --network cassandranetwork -p 7000:7000 -p 9042:9042 -d cassandra:latest
+docker run -it --network cassandranetwork --rm cassandra cqlsh some-cassandra
 
 cqlsh -f create_table.cql
 
